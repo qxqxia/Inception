276801decad93ea1c9a8d2c1ae5c0e8360cb6534
Your Name <xiaqindreamlady@gmail.com>
2023-01-28 17:24:25 +0100

    install wordpress everytime

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -9,7 +9,7 @@ define( 'DB_HOST', 'mariadb' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 define('FS_METHOD','direct');
-\$table_prefix = 'wp_';
+#\$table_prefix = 'wp_';
 $stabl_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 if ( ! defined( 'ABSPATH' ) ) {
